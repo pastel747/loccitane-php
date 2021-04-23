c19a81d7550b927980f2bd3de76153834205a63f
yeon <68599757+pastel747@users.noreply.github.com>
2021-04-23 23:34:18 +0900

    loccitane php test

---
+++ loccitane/inc/dbh.inc.php
@@ -0,0 +1,13 @@
+<!-- 데이터 베이스 핸들러 역할을 하는 php이다 -->
+<?php
+
+$serverName = "lacalhost";
+$dBuserName = "root";
+$dBpassword = "ny99ny";
+$dBname = "loccitane";
+
+$conn = mysqli_connect( $serverName, $dBuserName, $dBpassword, $dBname );
+// mysqli가 mysql의 최신형버전
+if(!$conn){
+    die('connection failed:' . mysqli_connect_error());
+}
