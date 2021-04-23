de686f3a85c23a401a15b50ee9ad0b2c1c5eb981
yeon <68599757+pastel747@users.noreply.github.com>
2021-04-23 23:38:57 +0900

    loccitane php test

--- loccitane/inc/dbh.inc.php
+++ loccitane/inc/dbh.inc.php
@@ -9,5 +9,5 @@ $dBname = "loccitane";
 $conn = mysqli_connect( $serverName, $dBuserName, $dBpassword, $dBname );
 // mysqli가 mysql의 최신형버전
 if(!$conn){
-    die('connection failed:' . mysqli_connect_error());
+    die('connection failed:' . mysqli_connect_error() );
 }
